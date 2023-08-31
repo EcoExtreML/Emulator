7b341316cb1a6ce581d949e91df57f7229fdc628
Qianqian Han <qiahan@int6.local.snellius.surf.nl>
2023-08-31 17:43:26 +0200

    update the jupyter notebook

--- launchJN.sh
+++ launchJN.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=fat
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=240GB
+#SBATCH --mem=256GB
   
 # Make sure the jupyter command is available, either by loading the appropriate modules, sourcing your own virtual environment, etc.
 module load 2021
