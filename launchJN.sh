902f7869fe3e04c253cd8876baea3f25078ddaf3
Qianqian Han <qiahan@int5.local.snellius.surf.nl>
2023-10-28 00:39:34 +0200

    check the notebooks for 30 degrees with netcdf era5land

--- launchJN.sh
+++ launchJN.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --time 1:00:00
+#SBATCH --time 2:00:00
 #SBATCH --nodes=1
 #SBATCH --partition=fat
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
-#SBATCH --mem=240GB
+#SBATCH --cpus-per-task=64
+#SBATCH --mem=480GB
   
 # Make sure the jupyter command is available, either by loading the appropriate modules, sourcing your own virtual environment, etc.
 module load 2021
